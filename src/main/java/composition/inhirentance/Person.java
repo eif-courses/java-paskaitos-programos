48005188bd1cda10f1427e36b9826af45b275ebd
Marius Gžegoževskis <m.gzegozevskis@gmail.com>
2017-06-21 12:51:37 +0300

    First update

--- src/main/java/composition/inhirentance/Person.java
+++ src/main/java/composition/inhirentance/Person.java
@@ -1,33 +1,14 @@
 package composition.inhirentance;
 
+import lombok.Data;
+
 import java.util.Arrays;
 
 /**
  * Created by Marius on 6/21/2017.
  */
-public class Person{
+public @Data
+class Person{
     private String name;
     private Role[] roles;
-
-    public Person(String name, Role[] roles) {
-        this.name = name;
-        this.roles = roles;
-    }
-    public Role[] getRoles() {
-        return roles;
-    }
-    public void setRoles(Role[] roles) {
-        this.roles = roles;
-    }
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", roles=" + Arrays.toString(roles) +
-                '}';
-    }
 }
--- src/main/java/inheritance/Person.java
+++ src/main/java/inheritance/Person.java
@@ -1,27 +1,15 @@
 package inheritance;
 
+import lombok.Data;
+
 /**
  * Created by Marius on 6/21/2017.
  */
-public class Person {
+public @Data class Person {
     private String name;
 
     public Person(String name) {
         this.name = name;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                '}';
-    }
+    
 }
